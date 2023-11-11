52e2ed19b0acc3ee4ba923e68bc10b6820f65c8a
romancode(c++, python) <roman.v.ua@gmail.com>
2023-11-11 17:39:08 +0200

    Implement others method, add validation

--- Task1/main.py
+++ Task1/main.py
@@ -1,7 +1,49 @@
-from worker import WorkerDB
+from worker import WorkerDB, Worker
 
 workers = WorkerDB()
 
 menu = """
-    1.
+    1. Add Worker
+    2. Delete Worker
+    3. Edit Worker
+    4. Read CSV
+    5. Display Workers
+    6. Sort Workers
+    7. Search Worker
 """
+
+while True:
+    print(menu)
+
+    choice = input("Select option: ")
+
+    if choice == '1':
+        worker = Worker.input_worker()
+        workers.add_worker(worker)
+    elif choice == '2':
+        try:
+            id_to_del = int(input("Enter id to delete: "))
+            workers.del_worker(id_to_del)
+        except ValueError as e:
+            print(e)
+    elif choice == '3':
+        try:
+            id_to_edit = int(input("Enter id to edit: "))
+            field = input("Enter field to edit: ")
+            value = input("Enter value to edit: ")
+            workers.edit_worker(id_to_edit, field, value)
+        except ValueError as e:
+            print(e)
+    elif choice == '4':
+        filename = input("Enter a filename: ")
+        workers.read_from_csv(filename)
+    elif choice == '5':
+        workers.display_workers()
+    elif choice == '6':
+        field = input("Enter field to sort: ")
+        workers.sort_workers(field)
+    elif choice == '7':
+        field = input("Enter field to search: ")
+        workers.search_workers(field)
+    else:
+        print("Invalid choice. Enter again.")
--- Task1/worker.py
+++ Task1/worker.py
@@ -1,13 +1,41 @@
 import csv
 
+c_id = 0
+
 
 class Worker:
     def __init__(self, name, surname, depart, salary):
-        self.__id
+        global c_id
+        self.__id = c_id
+        c_id = c_id + 1
+        self.set_id(c_id)
         self.name = name
         self.surname = surname
         self.depart = depart
-        self.salary = salary
+        self.salary = float(salary)
+
+    def set_id(self, new):
+        self.__id = new
+
+    def get_id(self):
+        return self.__id
+
+    def __str__(self):
+        output = ""
+        output += f"ID: {self.get_id()}\n"
+        output += f"Name: {self.name}\n"
+        output += f"Surname: {self.surname}\n"
+        output += f"Depart: {self.depart}\n"
+        output += f"Salary: {self.salary}\n"
+        return output
+
+    @classmethod
+    def input_worker(cls):
+        name = input("Enter worker name: ")
+        surname = input("Enter worker surname: ")
+        department = input("Enter worker department: ")
+        salary = float(input("Enter salary: "))
+        return cls(name, surname, department, salary)
 
 
 class WorkerDB:
@@ -17,21 +45,63 @@ class WorkerDB:
     def add_worker(self, worker):
         self.workers.append(worker)
 
-    def del_worker(self, worker):
-        self.workers.remove(worker)
+    def del_worker(self, id_to_del):
+        for worker in self.workers:
+            if worker.get_id() == id_to_del:
+                self.workers.remove(worker)
+                return
+            else:
+                print(f"Worker with ID {id_to_del} not found.")
+
+    def edit_worker(self, id_to_edit, field, value_to_edit):
+        for worker in self.workers:
+            if worker.get_id() == id_to_edit:
+                if hasattr(worker, field):
+                    setattr(worker, field, value_to_edit)
+                    print(f"Tax Free with ID {id_to_edit} - Field '{field}' edited.")
+                    return
+                else:
+                    print(f"Invalid field: {field}.")
+                return
+            else:
+                print(f"Worker with ID: {id_to_edit} not found.")
 
     def read_from_csv(self, filename):
         with open(filename, newline='') as csv_file:
             reader = csv.DictReader(csv_file, delimiter=',')
-            id = 0
             for row in reader:
-                if int(row['id']) > id:
-                    id = int(row['id'])
-                name, surname, depart, salary = row['name'], row['surname'], row['depart'], row['salary']
-                self.workers.append(Worker(name, surname, depart, salary))
+                name = row['name']
+                surname = row['surname']
+                depart = row['depart']
+                salary = float(row['salary'])
+                worker = Worker(name, surname, depart, salary)
+                self.add_worker(worker)
 
-    def write_to_csv(self, filename):
-        with open(filename, mode='w', newline='',) as csv_file:
-            writer = csv.writer(csv_file, delimiter=',')
-            for row in self.workers:
-                writer.writerow(row)
+    def display_workers(self):
+        if not self.workers:
+            print("List is empty.")
+        else:
+            for worker in self.workers:
+                print(worker)
+
+    def sort_workers(self, field):
+        if not self.workers:
+            print("List is empty.")
+        else:
+            try:
+                self.workers.sort(key=lambda item: getattr(item, field))
+                print(f"Sorted successfully by {field}")
+            except AttributeError as e:
+                print(e)
+
+    def search_workers(self, field):
+        found = []
+        for worker in self.workers:
+            if field in str(worker):
+                found.append(worker)
+        if not found:
+            print(f"Worker with field {field} not found.")
+        else:
+            print("Searched workers:")
+            for worker in found:
+                print(f"Worker with field '{field}':\n {worker}")
